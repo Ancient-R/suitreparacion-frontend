cfd3007d9802ac581cb2b8d57afcacefed8138c3
Ricardo Lima <navalimaricardo@gmail.com>
2022-01-11 16:47:10 -0600

    pagina de inicio de sesion

---
+++ src/pages/login/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// components
+import FormLogin from '../../components/forms/login/FormLogin';
+
+const LoginPage = () => {
+    return (
+
+        <div className='container'>
+            <FormLogin />
+        </div>
+    );
+}
+ 
+export default LoginPage;
