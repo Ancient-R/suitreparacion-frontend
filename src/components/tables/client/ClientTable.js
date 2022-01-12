7be9faee3f95d49f45e216c55fdf7dd89072e485
Ricardo Lima <navalimaricardo@gmail.com>
2022-01-12 11:47:56 -0600

    agregando tabla para mostrar clientes

---
+++ src/components/tables/client/ClientTable.js
@@ -0,0 +1,161 @@
+import React from 'react';
+
+import '../Tables.css';
+
+const ClientTable = () => {
+    return (
+        <div className='table__container'>
+            <div className='table__search'>
+                <i 
+                    className="fas fa-search table__search--icon bg-purple"
+                ></i>
+                <input 
+                    type="text" 
+                    className="table__search--input" 
+                    placeholder="Ej papi-02"
+                />
+            </div>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th className='table__head--row'>Nombre</th>
+                        <th className='table__head--row'>Dirección</th>
+                        <th className='table__head--row'>Acciones</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    <tr className="table__row">
+                        <td className="row__body">Maria Gomez Diaz</td>
+                        <td className="row__body">Calle 18 de marzo #21 avenida de las flores</td>
+                        <td className="row__body">
+                            <div className='actions'>
+                                <button className="action action-update">
+                                    <i className="fas fa-user-edit actions-update"></i>
+                                </button>
+                                <button className="action action-delete">
+                                    <i className="fas fa-user-times actions-delete"></i>
+                                </button>
+                            </div>
+                        </td>
+                        {/* <td className="row__body">
+                            <table className='table'>
+                                <tbody>
+                                    <tr>
+                                        <td className='row__body  row__body--internal'>Mackbook</td>
+                                        <td className='row__body row__body--internal'>En revision</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td className='row__body row__body--internal'>Laptop</td>
+                                        <td className='row__body row__body--internal'>Reparado</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td> */}
+                    </tr>
+
+                    <tr className="table__row">
+                        <td className="row__body">Maria Gomez Diaz</td>
+                        <td className="row__body">Calle 18 de marzo #21 avenida de las flores</td>
+                        <td className="row__body">
+                            <div className='actions'>
+                                <button className="action action-update">
+                                    <i className="fas fa-user-edit actions-update"></i>
+                                </button>
+                                <button className="action action-delete">
+                                    <i className="fas fa-user-times actions-delete"></i>
+                                </button>
+                            </div>
+                        </td>
+                        {/* <td className="row__body">
+                        <table className='table'>
+
+                                <tbody>
+
+                                    <tr>
+                                        <td className='row__body'>Laptop</td>
+                                        <td className='row__body'>Reparado</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td className='row__body'>Estereo</td>
+                                        <td className='row__body'>No reparado</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td> */}
+                    </tr>
+
+                    <tr className="table__row">
+                        <td className="row__body">Maria Gomez Diaz</td>
+                        <td className="row__body">Calle 18 de marzo #21 avenida de las flores</td>
+                        <td className="row__body">
+                            <div className='actions'>
+                                <button className="action action-update">
+                                    <i className="fas fa-user-edit actions-update"></i>
+                                </button>
+                                <button className="action action-delete">
+                                    <i className="fas fa-user-times actions-delete"></i>
+                                </button>
+                            </div>
+                        </td>
+                        {/* <td className="row__body">
+                            <table className='table'>
+
+                                <tbody>
+                                    <tr>
+                                        <td className='row__body'>Mackbook</td>
+                                        <td className='row__body'>En revision</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td className='row__body'>Laptop</td>
+                                        <td className='row__body'>Reparado</td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </td> */}
+                    </tr>
+
+                    <tr className="table__row">
+                        <td className="row__body">Maria Gomez Diaz</td>
+                        <td className="row__body">Calle 18 de marzo #21 avenida de las flores</td>
+                        <td className="row__body">
+                            <div className='actions'>
+                                <button className="action action-update">
+                                    <i className="fas fa-user-edit actions-update"></i>
+                                </button>
+                                <button className="action action-delete">
+                                    <i className="fas fa-user-times actions-delete"></i>
+                                </button>
+                            </div>
+                        </td>
+                        {/* <td className="row__body">
+                            <table className='table'>
+
+                                <tbody>
+                                    <tr>
+                                        <td className='row__body'>Mackbook</td>
+                                        <td className='row__body'>En revision</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td className='row__body'>Laptop</td>
+                                        <td className='row__body'>Reparado</td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </td> */}
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+    );
+}
+ 
+export default ClientTable;
