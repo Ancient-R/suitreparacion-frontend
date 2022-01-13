d86bf7acb27769ab02dac657c530ad489d8b7c51
Ricardo Lima <navalimaricardo@gmail.com>
2022-01-13 12:47:44 -0600

    primeros pasos redux, definiendo authReducer y combine reducers

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,14 @@
+
+const initialState = {
+    user: null,
+    permissions: null,
+    logged: false,
+}
+
+export const authReducer = ( state = initialState, action ) => {
+    switch( action.type ){
+
+        default:
+            return state
+    }
+}
