4797327c46ff3a74d5e2476f3343e2650eb0ae85
Ricardo Lima <navalimaricardo@gmail.com>
2022-01-13 14:30:19 -0600

    combinando reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,9 @@ import { combineReducers } from 'redux';
 
 // reducers
 import { authReducer } from './authReducer';
+import { alertReducer } from './alertReducer';
 
 export default combineReducers({
+    alert: alertReducer,
     auth: authReducer
 });
