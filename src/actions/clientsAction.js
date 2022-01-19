55eac25ed94c3059842abd83363fd42461d0fd5c
Ricardo Lima <navalimaricardo@gmail.com>
2022-01-19 14:56:29 -0600

    action y reducer para obtener los clientes, consultando la DB

--- src/actions/clientsAction.js
+++ src/actions/clientsAction.js
@@ -1,8 +1,49 @@
-import { } from '../types';
+import { MOSTRAR_ALERTA, OBTENER_CLIENTES_CORRECTO, OBTENER_CLIENTES_ERROR, OCULTAR_ALERTA } from '../types';
+
+// axios para consultas a la DB
+import clientAxios from '../axios/axios';
+
+// helper para mostrar alertas
+import { Alert } from '../helpers/Alert';
+
+// variable para la api
+const url = '/api/suitreparacion/clients';
 
 export const getClients = ( page = 1 ) => {
     
     return async ( dispatch ) => {
-        dispatch( console.log('Obteniendo usuarios... ') );
+        try {
+            
+            const res = await clientAxios.get(`${url}/get-clients/?page=${page}`);
+
+            if( res.data.ok ){
+
+                dispatch({
+                    type: OBTENER_CLIENTES_CORRECTO,
+                    payload: res.data.clients
+                });
+            }
+
+        } catch (error) {
+            const err = await error.response;
+
+            dispatch({
+                type: OBTENER_CLIENTES_ERROR
+            });
+
+            dispatch({
+                type: MOSTRAR_ALERTA,
+                payload: err.data.msg
+            });
+
+            Alert('¡Error!', err.data.msg, 'error');
+
+            setTimeout(() => {
+                dispatch({
+                    type: OCULTAR_ALERTA
+                });
+            }, 3000);
+
+        }
     }
 }
--- src/reducers/clientsReducer.js
+++ src/reducers/clientsReducer.js
@@ -1,4 +1,4 @@
-import { } from '../types';
+import { OBTENER_CLIENTES_CORRECTO, OBTENER_CLIENTES_ERROR } from '../types';
 
 const initialState = {
     clients: null,
@@ -14,6 +14,19 @@ const initialState = {
 export const clientsReducer = ( state = initialState, action ) => {
     switch( action.type ){
 
+        case OBTENER_CLIENTES_CORRECTO:
+            return {
+                clients: action.payload.docs,
+                actualPage: action.payload.page,
+                prevPage: action.payload.prevPage,
+                nextPage: action.payload.nextPage
+            }
+
+        case OBTENER_CLIENTES_ERROR:
+            return {
+                ...state
+            }
+            
         default:
             return state
     }
