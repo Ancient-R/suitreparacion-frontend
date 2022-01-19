755eaa212e4707232a7176005310520b14c83c94
Ricardo Lima <navalimaricardo@gmail.com>
2022-01-19 15:19:50 -0600

    definiendo state del formulario

--- src/components/forms/client/FormClient.js
+++ src/components/forms/client/FormClient.js
@@ -1,92 +1,133 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../Form.css';
 
 const FormClient = ({ isEdit }) => {
 
+    // estado del formulario
+    const [formClientValues, setFormClientValues] = useState({
+        name: '',
+        address: '',
+        phone: '',
+        email: '',
+    })
+
+    const { name, address, phone, email } = formClientValues;
+
+    // función para controlar los valores de los inputs
+    const handleInputChange = e => {
+        setFormClientValues({
+            ...formClientValues,
+            [ e.target.name ] : e.target.value
+        });
+    }
+
     return (
-        <form className='form form__user'>
-            <div className='form__fields--left'>
-                <div className='form__field'>
-                    <label
-                        className='form__label'
-                    >Nombre: </label>
-                    <div className='form__container--input'>
-                        <i className="fas fa-user-circle form__icon form__icon--info bg-purple "></i>
-                        <input 
-                            type="text"
-                            className='form__input form__input--info'
-                            placeholder="Ej. Leonardo Luna Mora"
-                        />
+        <>
+        { isEdit ? 
+            <h1 className='text-center'>Editar cliente</h1>
+            :
+            <h1 className='text-center'>Agregar cliente</h1>
+        }
+            <form className='form form__user'>
+                <div className='form__fields--left'>
+                    <div className='form__field'>
+                        <label
+                            className='form__label'
+                        >Nombre: </label>
+                        <div className='form__container--input'>
+                            <i className="fas fa-user-circle form__icon form__icon--info bg-purple "></i>
+                            <input 
+                                type="text"
+                                className='form__input form__input--info'
+                                placeholder="Ej. Leonardo Luna Mora"
+                                name='name'
+                                value={ name }
+                                onChange={ handleInputChange }
+                            />
+                        </div>
+                    </div>
+
+                    <div className='form__field'>
+                        <label
+                            className='form__label'
+                        >Dirección: </label>
+                        <div className='form__container--input'>
+                            <i className="fas fa-directions form__icon form__icon--info bg-purple "></i>
+                            <input 
+                                type="text"
+                                className='form__input form__input--info'
+                                placeholder='Ej. Calle de las flores #18'
+                                name='address'
+                                value={ address }
+                                onChange={ handleInputChange }
+                            />
+                        </div>
+                    </div>
+
+                    <div className='form__field'>
+                        <label
+                            className='form__label'
+                        >Teléfono: </label>
+                        <div className='form__container--input'>
+                            <i className="fas fa-phone form__icon form__icon--info bg-purple "></i>
+                            <input 
+                                type="text"
+                                className='form__input form__input--info'
+                                placeholder="10 digitos"
+                                name='phone'
+                                value={ phone }
+                                onChange={ handleInputChange }
+                            />
+                        </div>
                     </div>
                 </div>
 
-                <div className='form__field'>
-                    <label
-                        className='form__label'
-                    >Dirección: </label>
-                    <div className='form__container--input'>
-                        <i className="fas fa-directions form__icon form__icon--info bg-purple "></i>
-                        <input 
-                            type="text"
-                            className='form__input form__input--info'
-                            placeholder='Ej. Calle de las flores #18'
-                        />
+                <div className='form__fields--rigth'>
+                
+                    <div className='form__field'>
+                        <label
+                            className='form__label'
+                        >Correo: </label>
+                        <div className='form__container--input'>
+                            <i className="fas fa-at form__icon form__icon--info bg-purple "></i>
+                            <input 
+                                type="email"
+                                className='form__input form__input--info'
+                                placeholder='Ej. micorreo@correo.com'
+                                name='email'
+                                value={ email }
+                                onChange={ handleInputChange }
+                            />
+                        </div>
                     </div>
                 </div>
 
-                <div className='form__field'>
-                    <label
-                        className='form__label'
-                    >Teléfono: </label>
-                    <div className='form__container--input'>
-                        <i className="fas fa-phone form__icon form__icon--info bg-purple "></i>
-                        <input 
-                            type="text"
-                            className='form__input form__input--info'
-                            placeholder="10 digitos"
-                        />
-                    </div>
-                </div>
-            </div>
+                <div className='form__field--actions'>
 
-            <div className='form__fields--rigth'>
-            
-                <div className='form__field'>
-                    <label
-                        className='form__label'
-                    >Correo: </label>
-                    <div className='form__container--input'>
-                        <i className="fas fa-at form__icon form__icon--info bg-purple "></i>
-                        <input 
-                            type="email"
-                            className='form__input form__input--info'
-                            placeholder='Ej. micorreo@correo.com'
+                    { isEdit ? 
+                        <input
+                            type="submit"
+                            className='form__submit form__submit--info hover'
+                            value="Editar datos"
                         />
-                    </div>
-                </div>
-            </div>
 
-            { isEdit ? 
-                <div className='form__field'>
+                        :
+                        <input
+                            type="submit"
+                            className='form__submit form__submit--info hover'
+                            value="Agregar cliente"
+                        />
+                    }
                     <input
-                        type="submit"
-                        className='form__submit form__submit--info  bg-yellow  hover'
-                        value="Editar datos"
+                        type="button"
+                        className='form__submit form__submit--info hover'
+                        value="Limpiar formulario"
                     />
                 </div>
 
-                :
-
-                <div className='form__field'>
-                    <input
-                        type="submit"
-                        className='form__submit form__submit--info bg-green hover'
-                        value="Agregar cliente"
-                    />
-                </div>
-            }
-        </form>
+            </form>
+        </>
     );
 }
  
