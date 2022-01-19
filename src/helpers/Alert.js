43c502390f3c04970097fdca1f42df69a3af96fc
Ricardo Lima <navalimaricardo@gmail.com>
2022-01-19 11:18:18 -0600

    agregando alerta para eliminar usuario, dispatch al action deleteUser

--- src/helpers/Alert.js
+++ src/helpers/Alert.js
@@ -8,3 +8,25 @@ export const Alert = ( title, message, type ) => {
         type
     );
 }
+
+
+// muestra una alerta de advertencia para cuando se quiere eliminar un usuario, cliente o dispositivo
+
+    // recibe tres parametros un id, el dispatch y la función
+export const alertDelete = ( id, dispatch, deleteFun ) => {
+    Swal.fire({
+        title: '¿Estas seguro?',
+        text: "Esta acción es irreversible",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, eliminar',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            // paso por parametros el dispatch y la función de eliminar
+            dispatch( deleteFun( id ) );
+        }
+      })
+}
